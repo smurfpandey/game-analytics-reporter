78fd63d2305cdb80281120c2a95248a87b6a982b
Neeraj Verma <hello@smurfpandey.me>
2020-09-05 02:24:03 +0530

    poc done

--- IronPythonScript.py
+++ IronPythonScript.py
@@ -1,29 +1,32 @@
-def on_application_started():
-    pass
+from System.Net import WebRequest
+from System.IO import StreamReader
+from System.Text import Encoding
+import clr
 
-def on_application_stopped():
-    pass
+# clr.AddReferenceToFile("libs\\Newtonsoft.Json.dll")
+clr.AddReference('System.Web.Extensions')
+from System.Web.Script.Serialization import JavaScriptSerializer #since .net 3.5?
 
-def on_library_updated():
-    pass
+API_SERVER_URI = "http://requestbin.net/r/1ayyter1"
 
-def on_game_starting(game):
-    pass
+
+def PostData(jsonBody):
+    request = WebRequest.Create(API_SERVER_URI)
+    request.ContentType = "application/json"
+    request.Method = "POST"
+    bytes = Encoding.ASCII.GetBytes(jsonBody)
+    request.ContentLength = bytes.Length
+    reqStream = request.GetRequestStream()
+    reqStream.Write(bytes, 0, bytes.Length)
+    reqStream.Close()
+
+
 
 def on_game_started(game):
+    json=JavaScriptSerializer().Serialize(game)
+    PostData(json)
     pass
+
 
 def on_game_stopped(game, elapsed_seconds):
-    pass
-
-def on_game_installed(game):
-    pass
-
-def on_game_uninstalled(game):
-    pass
-
-def on_game_selected(args):
-    pass
-
-def menu_function():
     pass
